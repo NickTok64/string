33169e35685615467fd8f2c28944c325683ba56e
NickTok64 <bignick5515@gmail.com>
2024-10-28 21:44:34 +0300

    Add files via upload

---
+++ string.py
@@ -0,0 +1,6 @@
+example='Петербург'
+print(example[0]) # первый символ
+print(example[-1]) # последний символ
+print(example[4:9]) # вторая половина строки
+print(example[::-1]) # задом на перед
+print(example[1::2]) # каждый второй символ
